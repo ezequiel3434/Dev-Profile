95515e668952ea574740fa6455560b79e1dacf1b
Ezequiel <ezequiel3434@gmail.com>
2020-07-14 19:51:04 -0300

    first screen

---
+++ Dev-Profile/CustomUIImageView.swift
@@ -0,0 +1,23 @@
+//
+//  CustomUIImageView.swift
+//  Dev-Profile
+//
+//  Created by Ezequiel Parada Beltran on 14/07/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import UIKit
+
+class CustomUIImageView: UIImageView {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 10
+//        layer.shadowColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+//        layer.shadowRadius = 10
+//        layer.shadowOpacity = 0.75
+//        backgroundColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+//        layer.borderColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+//        layer.borderWidth = 5
+    }
+
+}
